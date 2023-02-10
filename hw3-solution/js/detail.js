6e34ad41d76609a70406dfa5faa3dd6ccac1597d
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-02-09 19:56:17 -0500

    Objects have been created

---
+++ hw3-solution/js/detail.js
@@ -0,0 +1,18 @@
+// DELETE LATER How I know I am successfully connected to detail page
+console.log('Welcome to the detail page');
+
+// Glazing Options
+const glazing_options = {
+    Keeporiginal: 0,
+    Sugarmilk: 0,
+    Vanillamilk: 0.50,
+    Doublechocolate: 1.50,
+}
+
+// Pack Size Options
+const pack_size_options = {
+    one: 1,
+    three: 3,
+    six: 5,
+    twelve: 10,
+}
