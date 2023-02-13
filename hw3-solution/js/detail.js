15d21997fbc592d6b2134cece33b7b2389224c86
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-02-12 20:58:47 -0500

    Populated drop Down Menus

--- hw3-solution/js/detail.js
+++ hw3-solution/js/detail.js
@@ -2,23 +2,15 @@ console.log('Welcome to the detail page');
 
 // Array of glazings and their price adaptions that will appear in pack size drop down menu
  let all_glazing = [
-//     // { 
-//     //     glazing_name: 'Keep Original',
-//     //     price_adaption: '0',
-//     // },
-//     // { 
-//     //     glazing_name: 'Sugar milk',
-//     //     price_adaption: '0',
-//     // },
-//     // { 
-//     //     glazing_name: 'Vanilla milk',
-//     //     price_adaption: '0.50',
-//     // },
-//     // { 
-//     //     glazing_name: 'Double Chocolate',
-//     //     price_adaption: '1.50',
-//     // }
 ];
+
+// function displayGlazing(glaze) {
+//     let carTitleElement = document.querySelector('#car-title');
+//     let carInfoElement = document.querySelector('#car-info');
+  
+//     carTitleElement.innerText = car.model;
+//     carInfoElement.innerText = car.description;
+//   }
 
 // Retrieving the drop down menu for glazing
 let glazingSelectElement = document.querySelector('#glazing-selector')
@@ -48,109 +40,42 @@ all_glazing.push(glazing_option_two)
 
 // Creating each option in the drop down menu in the UI
 var option = document.createElement('option');
-option.text = glazing_option_1.option_name;
+option.text = glazing_option_two.option_name;
 option.value = all_glazing.length - 1;
 glazingSelectElement.add(option);
 
 // Second Glazing Option: Sugar Milk
 let glazing_option_3 = {
-    option_name: 'Sugar Milk',
-    option_value: '0',
+    option_name: 'Vanilla Milk',
+    option_value: '0.50',
 }
 
 all_glazing.push(glazing_option_3)
 
 // Creating each option in the drop down menu in the UI
 var option = document.createElement('option');
-option.text = glazing_option_1.option_name;
+option.text = glazing_option_3.option_name;
 option.value = all_glazing.length - 1;
 glazingSelectElement.add(option);
 
 // Second Glazing Option: Sugar Milk
 let glazing_option_4 = {
-    option_name: 'Sugar Milk',
-    option_value: '0',
+    option_name: 'Double Chocolate',
+    option_value: '1.50',
 }
 
 all_glazing.push(glazing_option_4)
 
 // Creating each option in the drop down menu in the UI
 var option = document.createElement('option');
-option.text = glazing_option_1.option_name;
+option.text = glazing_option_4.option_name;
 option.value = all_glazing.length - 1;
 glazingSelectElement.add(option);
 
-
-
-// Array of pack sizes and their price adaptions that will appear in pack size drop down menu
-let all_pack_size = [
-    // All possible options that will be dynamically added to this array
-    // { 
-    //     pack_size_name: '1',
-    //     price_adaption: '1',
-    // },
-    // { 
-    //     pack_size_name: '3',
-    //     price_adaption: '3',
-    // },
-    // { 
-    //     pack_size_name: '6',
-    //     price_adaption: '5',
-    // },
-    // { 
-    //     pack_size_name: '12',
-    //     price_adaption: '10',
-    // }
-]; 
-
-
-
-// Retrieving the drop down menu for pack size
-let packSizeSelectElement = document.querySelector('#pack-size-selector')
-
-// Add a new glazing to the drop down menu
-// I believe here I could use a loop to populate drop down for each item
-for (let i = 0; i < all_glazing.length; i++)
-{
-    let glazing_options = {
-        Keeporiginal: 0,
-        Sugarmilk: 0,
-        Vanillamilk: 0.50,
-        Doublechocolate: 1.50,
-    }
-    // let all_glazing = []
-
-    all_glazing.push(glazing_options)
-
-    console.log('A glazing has been added');
-
-}
-
-
-// Pack Size Options
-
-
-
-
-
-
-
-
-
-
-// Add each pack size to the drop down menu
-// for (){
-//     console.log('A pack size has been added')
-//     let newPackSize = {
-//         pack_size_name: 'Test Double Chocolate',
-//         price_adaption: '21.50',
-//     };
-//     all_pack_size.push(newPackSize)    
-// }
-
-function onSelectValueChange() {
+function glazingOnSelectValueChange() {
     console.log('You selected' + this.value);
 
+    console.log(glazingIndex)
     let glazingIndex = all_glazing[glazingIndex];
 
     let glazingToDisplay = parseInt(this.value);
@@ -158,8 +83,87 @@ function onSelectValueChange() {
     displayGlazing(glazingToDisplay);
 }
 
-glazingSelectElement.addEventListener('change', onSelectValueChange);
-packSizeSelectElement.addEventListener('change', onSelectValueChange);
+glazingSelectElement.addEventListener('change', glazingOnSelectValueChange);
 
-// If array is drop down options, 
+// Array of pack sizes and their price adaptions that will appear in pack size drop down menu
+let all_pack_size = [
+]; 
+
+// function displayPackSize(pack_size) {
+//     let packSizeTitleElement = document.querySelector('#car-title');
+//     let carInfoElement = document.querySelector('#car-info');
+  
+//     carTitleElement.innerText = car.model;
+//     carInfoElement.innerText = car.description;
+//   }
+
+// Retrieving the drop down menu for pack size
+let packSizeSelectElement = document.querySelector('#pack-size-selector')
+
+// First Glazing Option: Keep Original
+let pack_size_option_1 = {
+    option_name:'1',
+    option_value:'1',
+}
+
+all_pack_size.push(pack_size_option_1)
+
+// Creating each option in the drop down menu in the UI
+var option = document.createElement('option');
+option.text = pack_size_option_1.option_name;
+option.value = all_pack_size.length - 1;
+packSizeSelectElement.add(option);
+
+let pack_size_option_two = {
+    option_name:'3',
+    option_value:'3',
+}
+
+all_pack_size.push(pack_size_option_two)
+
+// Creating each option in the drop down menu in the UI
+var option = document.createElement('option');
+option.text = pack_size_option_two.option_name;
+option.value = all_pack_size.length - 1;
+packSizeSelectElement.add(option);
+
+let pack_size_option_three= {
+    option_name:'6',
+    option_value:'5',
+}
+
+all_pack_size.push(pack_size_option_three)
+
+// Creating each option in the drop down menu in the UI
+var option = document.createElement('option');
+option.text = pack_size_option_three.option_name;
+option.value = all_pack_size.length - 1;
+packSizeSelectElement.add(option);
+
+let pack_size_option_four= {
+    option_name:'12',
+    option_value:'10',
+}
+
+all_pack_size.push(pack_size_option_four)
+
+// Creating each option in the drop down menu in the UI
+var option = document.createElement('option');
+option.text = pack_size_option_four.option_name;
+option.value = all_pack_size.length - 1;
+packSizeSelectElement.add(option);
+
+function packSizeOnSelectValueChange() {
+    console.log('You selected ' + this.value);
+    console.log(packSizeIndex)
+
+    let packSizeIndex = all_pack_size[packSizeIndex];
+
+    let packSizeToDisplay = parseInt(this.value);
+
+    displayPackSize(packSizeToDisplay);
+}
+
+packSizeSelectElement.addEventListener('change', packSizeOnSelectValueChange);
+
 
