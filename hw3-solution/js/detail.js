c87d06975049e2d8d603fabe5fcc7af094567880
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-02-14 22:16:56 -0500

    Populated drop down using loops!

--- hw3-solution/js/detail.js
+++ hw3-solution/js/detail.js
@@ -15,136 +15,41 @@ const packSizeOptions = {
 }
 
 // Array of glazings and their price adaptions that will appear in pack size drop down menu
- let all_glazing = [
-];
-
-// Retrieving the drop down menu for glazing
-let glazingSelectElement = document.querySelector('#glazing-selector')
-
-// First Glazing Option: Keep Original
-let glazing_option_1 = {
-    option_name:'Keep Original',
-    option_value: '0',
-}
-
-all_glazing.push(glazing_option_1)
-
-// Creating each option in the drop down menu in the UI
-var option = document.createElement('option');
-option.text = glazing_option_1.option_name;
-option.value = all_glazing.length - 1;
-glazingSelectElement.add(option);
-
-
-// Second Glazing Option: Sugar Milk
-let glazing_option_two = {
-    option_name: 'Sugar Milk',
-    option_value: '0',
-}
-
-all_glazing.push(glazing_option_two)
-
-// Creating each option in the drop down menu in the UI
-var option = document.createElement('option');
-option.text = glazing_option_two.option_name;
-option.value = all_glazing.length - 1;
-glazingSelectElement.add(option);
-
-// Second Glazing Option: Sugar Milk
-let glazing_option_3 = {
-    option_name: 'Vanilla Milk',
-    option_value: '0.50',
-}
-
-all_glazing.push(glazing_option_3)
-
-// Creating each option in the drop down menu in the UI
-var option = document.createElement('option');
-option.text = glazing_option_3.option_name;
-option.value = all_glazing.length - 1;
-glazingSelectElement.add(option);
-
-// Second Glazing Option: Sugar Milk
-let glazing_option_4 = {
-    option_name: 'Double Chocolate',
-    option_value: '1.50',
-}
-
-all_glazing.push(glazing_option_4)
-
-// Creating each option in the drop down menu in the UI
-var option = document.createElement('option');
-option.text = glazing_option_4.option_name;
-option.value = all_glazing.length - 1;
-glazingSelectElement.add(option);
-
-function glazingOnSelectValueChange() {
-    console.log('You selected' + this.value);
-}
-
-glazingSelectElement.addEventListener('change', glazingOnSelectValueChange);
-
-
+ let all_glazing = [];
 
 // Array of pack sizes and their price adaptions that will appear in pack size drop down menu
-let all_pack_size = [
-]; 
+let all_pack_size = []; 
+
+// Accessing the glazing options' drop down menu
+let glazingSelectElement = document.querySelector('#glazing-selector')
+
+// For loop that adds glazing options to glazing drop down menu
+for (i in glazingOptions) {
+    all_glazing.push(i);
+    var option = document.createElement('option');
+    option.text = i;
+    glazingSelectElement.add(option);
+}
+
+// When select element changes, the final price is recalculated
+function glazingOnSelectValueChange() {
+    console.log('You selected ' + this.value);
+    // calculatePrice();
+}
+
+// Looking/listening for user's selection on the drop down menu
+glazingSelectElement.addEventListener('change', glazingOnSelectValueChange);
 
 // Retrieving the drop down menu for pack size
 let packSizeSelectElement = document.querySelector('#pack-size-selector')
 
-// First Glazing Option: Keep Original
-let pack_size_option_1 = {
-    option_name:'1',
-    option_value:'1',
+// For loop for adding pack sizes to pack size drop down menu
+for (i in packSizeOptions) {
+    all_pack_size.push(i);
+    var option = document.createElement('option');
+    option.text = i;
+    packSizeSelectElement.add(option);
 }
-
-all_pack_size.push(pack_size_option_1)
-
-// Creating each option in the drop down menu in the UI
-var option = document.createElement('option');
-option.text = pack_size_option_1.option_name;
-option.value = all_pack_size.length - 1;
-packSizeSelectElement.add(option);
-
-let pack_size_option_two = {
-    option_name:'3',
-    option_value:'3',
-}
-
-all_pack_size.push(pack_size_option_two)
-
-// Creating each option in the drop down menu in the UI
-var option = document.createElement('option');
-option.text = pack_size_option_two.option_name;
-option.value = all_pack_size.length - 1;
-packSizeSelectElement.add(option);
-
-let pack_size_option_three= {
-    option_name:'6',
-    option_value:'5',
-}
-
-all_pack_size.push(pack_size_option_three)
-
-// Creating each option in the drop down menu in the UI
-var option = document.createElement('option');
-option.text = pack_size_option_three.option_name;
-option.value = all_pack_size.length - 1;
-packSizeSelectElement.add(option);
-
-let pack_size_option_four= {
-    option_name:'12',
-    option_value:'10',
-}
-
-all_pack_size.push(pack_size_option_four)
-
-// Creating each option in the drop down menu in the UI
-var option = document.createElement('option');
-option.text = pack_size_option_four.option_name;
-option.value = all_pack_size.length - 1;
-packSizeSelectElement.add(option);
 
 function packSizeOnSelectValueChange() {
     console.log('You selected ' + this.value);
@@ -157,12 +62,27 @@ function glazingChange(element) {
     // get the value (price adaption) of selected element
     const priceChange = element.value;
     return priceChange;
+    calculatePrice(glazingPrice, packPrice);
+
 }
+
+var glazingPrice = glazingChange(element)
+console.log('Your glazing price is ' + glazingPrice)
 
 function packSizeChange(element) {
     // get the value (price adaption) of selected element
+    console.log(this.element);
     const packPrice = element.value;
     // update the final price
+    calculatePrice(glazingPrice, packPrice);
+}
+var packSizePrice = packSizeChange(element)
+console.log('Your pack size price is ' + packSizePrice)
+
+function calculatePrice(glazingPrice, packSizePrice) {
+    // Select what the current glazing option is and get it's value
+    // Select what the current pack size option is and get it's value
+    // Use the formula and change the inner text of product detail's price
     const basePrice = '2.49';
     console.log(glazingChange(element));
     console.log(packPrice);
@@ -170,5 +90,14 @@ function packSizeChange(element) {
     const productDetailPrice = document.querySelector('.detail-price');
     console.log(' The current final price of this product is ' + finalPrice);
     productDetailPrice.innerText = finalPrice;
+
+
+}
+
+function updatePrice() {
+    // Select what the current glazing option is and get it's value
+    // Select what the current pack size option is and get it's value
+    // Use the formula and change the inner text of product detail's price
+
 
 }
