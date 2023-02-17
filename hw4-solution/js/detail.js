3ac88dbb64384f079ddaf7e7b5f78c3e32be0c43
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-02-16 22:52:39 -0500

    All links and pics highlight yellow

--- hw4-solution/js/detail.js
+++ hw4-solution/js/detail.js
@@ -87,31 +87,3 @@ function calculatePrice() {
     productDetailPrice.innerText = finalPrice.toFixed(2);
 }
 
-// URL Params
-
-// Array that will house products intended to be checkedout
-const cart = [];
-
-// Gather info from URL
-const queryString = window.location.search;
-console.log(queryString);
-
-// Creating a new query string with created url
-const params = new URLSearchParams(queryString);
-console.log(params);
-
-// Gets the current roll type and stores it in a variable
-const rollType = params.get('roll');
-console.log(rollType);
-
-// Update Detail Page's Header text based on chosen product
-const headerElement = document.querySelector('.detail-heading');
-// Change text of header element based on chosen product
-headerElement.innerText = 'rollType';
-
-// Update Image based on selected product
-const productDetailImage = document.querySelector('.detail-pics');
-productDetailImage.src = './bakery_products/' + rollType + '.png';
-// Updates the alt text of image to match the selected product
-productDetailImage.alt = rollType;
-
