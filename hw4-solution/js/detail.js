4fe72097ba74e800e7f73dc60dc70a018e52dabe
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-02-20 17:15:10 -0500

    Updated Detail Page

--- hw4-solution/js/detail.js
+++ hw4-solution/js/detail.js
@@ -1,4 +1,30 @@
-import { productBasePrice } from "./rollsData.js";
+// Rolls Data
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "bakery_products/original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "bakery_products/apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "bakery_products/raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "bakery_products/walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "bakery_products/double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "bakery_products/strawberry-cinnamon-roll.jpg"
+    }    
+};
 
 // Object for glazings
 const glazingOptions = {
@@ -66,62 +92,11 @@ packSizeSelectElement.addEventListener('change', packSizeOnSelectValueChange);
 var glazingPrice = '0';
 
 // Default Price of the selected pack size option
-var packPrice = '0';
-
-// Get's the current glazing option's price and change's the final price based on user's selection
-function glazingChange(element) {
-    glazingPrice = parseFloat(element.value);
-    calculatePrice();
-}
-
-// Get's the current pack size price and change's the final price based on user's selection
-function packSizeChange(element) {    
-    packPrice = parseInt(element.value);
-    calculatePrice();
-}
-
-
-
-// Calculates and updates the final price on the product detail page
-function calculatePrice() {
-    // const productBasePrice = document.querySelector('.detail-price');
-    const finalPrice = (parseFloat(productBasePrice) + glazingPrice) * packPrice;
-    const productDetailPrice = document.querySelector('.detail-price');
-    // Credits .toFixed(): https://www.w3schools.com/jsref/jsref_tofixed.asp#:~:text=The%20toFixed()%20method%20rounds,a%20specified%20number%20of%20decimals.
-    productDetailPrice.innerText = finalPrice.toFixed(2);
-};
-
-// Rolls Data
-const rolls = {
-    "Original": {
-        "basePrice": 2.49,
-        "imageFile": "bakery_products/original-cinnamon-roll.jpg"
-    },
-    "Apple": {
-        "basePrice": 3.49,
-        "imageFile": "bakery_products/apple-cinnamon-roll.jpg"
-    },
-    "Raisin": {
-        "basePrice": 2.99,
-        "imageFile": "bakery_products/raisin-cinnamon-roll.jpg"
-    },
-    "Walnut": {
-        "basePrice": 3.49,
-        "imageFile": "bakery_products/walnut-cinnamon-roll.jpg"
-    },
-    "Double-Chocolate": {
-        "basePrice": 3.99,
-        "imageFile": "bakery_products/double-chocolate-cinnamon-roll.jpg"
-    },
-    "Strawberry": {
-        "basePrice": 3.99,
-        "imageFile": "bakery_products/strawberry-cinnamon-roll.jpg"
-    }    
-};
+var packPrice = '1';
 
 // URL Params
 
-// Array that will house products intended to be checkedout
+// Array that will house products intended to be checked out
 const cart = [];
 
 // Gather info from URL
@@ -138,21 +113,49 @@ console.log(rollType);
 
 // Update Detail Page's Header text based on chosen product
 const headerElement = document.querySelector('.detail-heading');
+
 // Change text of header element based on chosen product
 headerElement.innerText = rollType + ' Cinnamon Roll';
 
 // Update Image based on selected product
 const productDetailImage = document.querySelector('.detail-pics');
+
 // Credits String.toLowerCase(): https://www.freecodecamp.org/news/javascript-tolowercase-how-to-convert-a-string-to-lowercase-and-uppercase-in-js/
 productDetailImage.src = 'bakery_products/' + rollType.toLowerCase() + '-' + 'cinnamon' + '-' + 'roll'+ '.jpg';
 console.log(productDetailImage.src);
+
 // Updates the alt text of image to match the selected product
 productDetailImage.alt = rollType + " Cinnamon Roll";
 
-// 
+// Selecting area of a product's base price
 const productBasePrice = document.querySelector('.detail-price');
-// default Price of product shown on detail page
+
+// Changing default price of product shown on detail page
 productBasePrice.innerText = rolls[rollType]['basePrice'];
 
-export {productBasePrice};
+// Get's the current glazing option's price and change's the final price based on user's selection
+function glazingChange(element) {
+    glazingPrice = parseFloat(element.value);
+    calculatePrice();
+};
+
+// Get's the current pack size price and change's the final price based on user's selection
+function packSizeChange(element) {    
+    packPrice = parseInt(element.value);
+    calculatePrice();
+};
+
+// Calculates and updates the final price on the product detail page
+function calculatePrice() {
+    const basePrice = parseFloat(rolls[rollType]['basePrice']);
+    console.log(basePrice);
+    const finalPrice = (basePrice + glazingPrice) * packPrice;
+    console.log(finalPrice);
+    const productDetailPrice = document.querySelector('.detail-price');
+    // Credits .toFixed(): https://www.w3schools.com/jsref/jsref_tofixed.asp#:~:text=The%20toFixed()%20method%20rounds,a%20specified%20number%20of%20decimals.
+    productDetailPrice.innerText = finalPrice.toFixed(2);
+};
+
+
+
 
