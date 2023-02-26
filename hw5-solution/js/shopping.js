abd851e776b621937a201f0801e885c0054f7d43
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-02-26 01:10:16 -0500

    Added to cart

--- hw5-solution/js/shopping.js
+++ hw5-solution/js/shopping.js
@@ -1,8 +1,9 @@
+
 // Array that will house products intended to be checked out
 const shoppingCart = [];
 
 // Roll Class used to save all of the current product Information
-class Roll {
+class Roll_Class {
     constructor(rollType, rollGlazing, packSize, rollPrice) {
         this.type = rollType;
         this.glazing =  rollGlazing;
@@ -11,28 +12,34 @@ class Roll {
     }
 };
 
-const rollOne = createRoll(
+function addNewRoll(rollType, rollGlazing, packSize, rollPrice) {
+    const shoppingRoll = new Roll_Class(rollType, rollGlazing, packSize, rollPrice);
+    shoppingCart.push(shoppingRoll);
+    return shoppingRoll;
+};
+
+const rollOne = addNewRoll(
     'Original',
     'Sugar Milk',
     '1',
     '2.49'
 );
 
-const rollTwo = createRoll (
+const rollTwo = addNewRoll (
     'Walnut',
     'Vanilla Milk',
     '12',
     '3.49'
 );
 
-const rollThree = createRoll (
+const rollThree = addNewRoll (
     'Raisin',
     'Sugar Milk',
     '3',
     '2.99'
 );
 
-const rollFour = createRoll (
+const rollFour = addNewRoll (
     'Apple',
     'Original',
     '3',
