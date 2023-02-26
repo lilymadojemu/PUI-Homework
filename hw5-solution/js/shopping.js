5478c964ec9a9caff5f1907a28c616a8011b6c54
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-02-26 17:02:44 -0500

    Finished Initializing the shopping cart

--- hw5-solution/js/shopping.js
+++ hw5-solution/js/shopping.js
@@ -1,3 +1,46 @@
+// Rolls Data
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "bakery_products/original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "bakery_products/apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "bakery_products/raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "bakery_products/walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "bakery_products/double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "bakery_products/strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+// Object for glazings
+const glazingOptions = {
+    'Keep Original':0.0,
+    'Sugar Milk': 0.0,
+    'Vanilla Milk': 0.50,
+    'Double Chocolate': 1.50,
+};
+
+// Object for pack sizes
+const packSizeOptions = {
+    '1': 1.0,
+    '3': 3.0,
+    '6': 5.0,
+    '12': 10.0,
+};
 
 // Array that will house products intended to be checked out
 const shoppingCart = [];
@@ -12,6 +55,21 @@ class Roll_Class {
     }
 };
 
+// Gather info from URL
+const queryString = window.location.search;
+console.log(queryString);
+
+// Creating a new query string with created url
+const params = new URLSearchParams(queryString);
+console.log(params);
+
+const rollType = params.get('roll');
+
+// Calculates and updates the final price on the product detail page
+function shoppingCalculatePrice(basePrice, glazingPrice, packPrice) {
+    const finalPrice = (basePrice+ glazingPrice) * packPrice;
+    return finalPrice.toFixed(2);
+};
 function addNewRoll(rollType, rollGlazing, packSize, rollPrice) {
     const shoppingRoll = new Roll_Class(rollType, rollGlazing, packSize, rollPrice);
     shoppingCart.push(shoppingRoll);
@@ -22,26 +80,26 @@ const rollOne = addNewRoll(
     'Original',
     'Sugar Milk',
     '1',
-    '2.49'
+    shoppingCalculatePrice(rolls['Original']['basePrice'], glazingOptions['Sugar Milk'], packSizeOptions['1'])
 );
 
 const rollTwo = addNewRoll (
     'Walnut',
     'Vanilla Milk',
     '12',
-    '3.49'
+    shoppingCalculatePrice(rolls['Walnut']['basePrice'], glazingOptions['Vanilla Milk'], packSizeOptions['12'])
 );
 
 const rollThree = addNewRoll (
     'Raisin',
     'Sugar Milk',
     '3',
-    '2.99'
+    shoppingCalculatePrice(rolls['Raisin']['basePrice'], glazingOptions['Sugar Milk'], packSizeOptions['3'])
 );
 
 const rollFour = addNewRoll (
     'Apple',
     'Original',
     '3',
-    '3.49'
+    shoppingCalculatePrice(rolls['Apple']['basePrice'], glazingOptions['Keep Original'], packSizeOptions['3'])
 );
